e582b9ad898d668c46aebaa70b2035b5de1bceb0
Mohd Aasan <104296229+ofcyouritachii@users.noreply.github.com>
2025-10-17 12:55:22 +0530

    updated 5th.c

---
+++ 5th.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+    int a , num, n, sum=0;
+    printf("Enter a 5 digit number: ");
+    scanf("%d", &num);
+    a = num%10;
+    n = num/10;
+    sum = sum+a;
+    a = n%10;
+    n = n/10;
+    sum = sum+a;
+    a = n%10;
+    n = n/10;
+    sum = sum+a;
+    a = n%10;
+    n = n/10;
+    sum = sum+a;
+    a=n%10;
+    sum = sum+a;
+    printf("Sum of 5 digit %d=%d", num, sum);
+    
+    return 0;
+}
