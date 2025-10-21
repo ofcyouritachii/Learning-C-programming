3cb3cfb60a0755c087888e8833f0b711582dd940
Mohd Aasan <104296229+ofcyouritachii@users.noreply.github.com>
2025-10-21 12:47:42 +0530

    new program

---
+++ 6th.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    float angle1, angle2, angle3;
+    printf("Enter The 1st value for Angle1:");
+    scanf("%f", &angle1);
+    printf("Enter The 2nd value for Angle2:");
+    scanf("%f", &angle2);
+    printf("Enter The 3rd value for Angle3:");
+    scanf("%f", &angle3);
+    if((angle1+angle2+angle3)==180)
+    printf("The triangle has valid angles");
+    else
+    printf("The triangle has invalid angles");
+    return 0;
+}
