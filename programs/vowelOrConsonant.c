5439583292b3563380114a6456015fcff6577033
Mohd Asan <104296229+ofcyouritachii@users.noreply.github.com>
2025-11-04 11:04:47 +0530

    update

--- programs/vowelOrConsonant.c
+++ programs/vowelOrConsonant.c
@@ -1,24 +1,16 @@
 #include <stdio.h>
-#include <ctype.h>
 
-int main(void)
-{
+int main() {
     char ch;
-    printf("Enter a single character: ");
-    if (scanf(" %c", &ch) != 1) {
-        fprintf(stderr, "No input provided.\n");
-        return 1;
-    }
-
-    if (!isalpha((unsigned char)ch)) {
-        printf("'%c' is not an alphabetic character.\n", ch);
-        return 0;
-    }
-    char lower = (char)tolower((unsigned char)ch);
-    if (lower == 'a' || lower == 'e' || lower == 'i' || lower == 'o' || lower == 'u') {
-        printf("The character %c is a vowel.\n", ch);
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+        ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+        printf("%c is a vowel.\n", ch);
+    } else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+        printf("%c is a consonant.\n", ch);
     } else {
-        printf("The character %c is a consonant.\n", ch);
+        printf("%c is not a letter.\n", ch);
     }
 
     return 0;
