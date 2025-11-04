04dab38c2f08ce68d5d29e585e9d84c5186465e4
Mohd Asan <104296229+ofcyouritachii@users.noreply.github.com>
2025-11-04 11:24:37 +0530

    .

--- programs/vowelOrConsonant.c
+++ programs/vowelOrConsonant.c
@@ -7,11 +7,8 @@ int main() {
     if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
         ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
         printf("%c is a vowel.\n", ch);
-    } else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+    } else{
         printf("%c is a consonant.\n", ch);
-    } else {
-        printf("%c is not a letter.\n", ch);
     }
-
     return 0;
 }
