c270974dafbf0698380e07c3b25db516279a7c7e
Mohd Asan <104296229+ofcyouritachii@users.noreply.github.com>
2025-11-14 15:33:33 +0000

    Refactor variable names in calculator program for consistency

--- programs/calculator.c
+++ programs/calculator.c
@@ -2,29 +2,28 @@
 
 int main() {
     char operator;
-    double num1, num2;
-
+    double num, num2;
 
     printf("Simple Calculator in C\n");
     printf("Enter operator (+, -, *, /): ");
     scanf("%c", &operator);
 
     printf("Enter two numbers: ");
-    scanf("%lf %lf", &num1, &num2);
+    scanf("%lf %lf", &num, &num2);
     switch(operator) {
         case '+':
-            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2);
+            printf("%.2lf + %.2lf = %.2lf\n", num, num2, num + num2);
             break;
         case '-':
-            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2);
+            printf("%.2lf - %.2lf = %.2lf\n", num, num2, num - num2);
             break;
         case '*':
-            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
+            printf("%.2lf * %.2lf = %.2lf\n", num, num2, num * num2);
             break;
         case '/':
 		
             if (num2 != 0) {
-                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
+                printf("%.2lf / %.2lf = %.2lf\n", num, num2, num / num2);
             } else {
                 printf("Error! Division by zero.\n");
             }
